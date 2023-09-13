b9e601ace82339d6e58c9c378426a3d8232f6e98
laliux <eduardo@bbfour.com>
2023-09-13 17:15:40 +0000

    Added code to switch between media devices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,29 +9,70 @@ import Peer from "simple-peer"
 import io from "socket.io-client"
 import "./App.css"
 
+const fieldStyle = {
+	display: 'flex'
+};
 
-const socket = io.connect('http://localhost:5000')
+const labelStyle = {
+	flex: '0 110px'
+};
+
+const selectStyle = {
+	flex: 'auto'
+};
+
+const socket = io.connect('https://socket.bbfour.me')
 function App() {
-	const [ me, setMe ] = useState("")
-	const [ stream, setStream ] = useState()
-	const [ receivingCall, setReceivingCall ] = useState(false)
-	const [ caller, setCaller ] = useState("")
-	const [ callerSignal, setCallerSignal ] = useState()
-	const [ callAccepted, setCallAccepted ] = useState(false)
-	const [ idToCall, setIdToCall ] = useState("")
-	const [ callEnded, setCallEnded] = useState(false)
-	const [ name, setName ] = useState("")
+	const [videoDeviceId, setVideoDeviceId] = React.useState(null);
+	const [audioDeviceId, setAudioDeviceId] = React.useState(null);
+	const [devices, setDevices] = React.useState(null);
+	const [me, setMe] = useState("")
+	const [stream, setStream] = useState()
+	const [receivingCall, setReceivingCall] = useState(false)
+	const [caller, setCaller] = useState("")
+	const [callerSignal, setCallerSignal] = useState()
+	const [callAccepted, setCallAccepted] = useState(false)
+	const [idToCall, setIdToCall] = useState("")
+	const [callEnded, setCallEnded] = useState(false)
+	const [name, setName] = useState("")
 	const myVideo = useRef()
 	const userVideo = useRef()
-	const connectionRef= useRef()
+	const connectionRef = useRef()
+
+	const listDevices = async () => {
+		const devices = await navigator.mediaDevices?.enumerateDevices?.();
+		if (devices) {
+			const video = [];
+			const audio = [];
+			for (const device of devices) {
+				switch (device.kind) {
+					case 'videoinput':
+						video.push(device);
+						break;
+					case 'audioinput':
+						audio.push(device);
+						break;
+					default:
+						break;
+				}
+			}
+			return { video, audio };
+		} else {
+			throw new Error('No support for multimedia devices.');
+		}
+	};
 
 	useEffect(() => {
+		const promise = listDevices();  // <--- lists available input audio and input video devices
+		promise
+			.then((devices) => setDevices(devices));
+
 		navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
 			setStream(stream)
-				myVideo.current.srcObject = stream
+			myVideo.current.srcObject = stream
 		})
 
-	socket.on("me", (id) => {
+		socket.on("me", (id) => {
 			setMe(id)
 		})
 
@@ -41,7 +82,23 @@ function App() {
 			setName(data.name)
 			setCallerSignal(data.signal)
 		})
-	}, [])
+
+		const handleDeviceChange = () => {
+			console.log("Calling handleDeviceChange() ", audioDeviceId, videoDeviceId);
+			navigator.mediaDevices.getUserMedia({
+				video: videoDeviceId ? { deviceId: videoDeviceId } : true,
+				audio: audioDeviceId ? { deviceId: audioDeviceId } : true
+			}).then((stream) => {
+				setStream(stream)
+				myVideo.current.srcObject = stream
+			})
+		};
+
+		if (videoDeviceId || audioDeviceId) {
+			handleDeviceChange();
+		}
+
+	}, [videoDeviceId, audioDeviceId])
 
 	const callUser = (id) => {
 		const peer = new Peer({
@@ -58,9 +115,9 @@ function App() {
 			})
 		})
 		peer.on("stream", (stream) => {
-			
-				userVideo.current.srcObject = stream
-			
+
+			userVideo.current.srcObject = stream
+
 		})
 		socket.on("callAccepted", (signal) => {
 			setCallAccepted(true)
@@ -70,7 +127,7 @@ function App() {
 		connectionRef.current = peer
 	}
 
-	const answerCall =() =>  {
+	const answerCall = () => {
 		setCallAccepted(true)
 		const peer = new Peer({
 			initiator: false,
@@ -93,66 +150,105 @@ function App() {
 		connectionRef.current.destroy()
 	}
 
+	const handleVideoDeviceChange = (e) => {
+		const device = devices.video[e.target.value];
+		console.log(`Video device change:\n  label: ${device.label}\n  deviceId: ${device.deviceId}\n  groupId: ${device.groupId}`);
+
+		setVideoDeviceId(device.deviceId);
+	};
+	const handleAudioDeviceChange = (e) => {
+		const device = devices.audio[e.target.value];
+		console.log(`Audio device change:\n  label: ${device.label}\n  deviceId: ${device.deviceId}\n  groupId: ${device.groupId}`);
+
+		setAudioDeviceId(device.deviceId);
+	};
+
 	return (
 		<>
 			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
-		<div className="container">
-			<div className="video-container">
-				<div className="video">
-					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+			<div className="container">
+				<div className="video-container">
+					<div className="video">
+						{stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+					</div>
+					<div className="video">
+						{callAccepted && !callEnded ?
+							<video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} /> :
+							null}
+					</div>
 				</div>
-				<div className="video">
-					{callAccepted && !callEnded ?
-					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
-					null}
-				</div>
-			</div>
-			<div className="myId">
-				<TextField
-					id="filled-basic"
-					label="Name"
-					variant="filled"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					style={{ marginBottom: "20px" }}
-				/>
-				<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-					<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
-						Copy ID
-					</Button>
-				</CopyToClipboard>
-
-				<TextField
-					id="filled-basic"
-					label="ID to call"
-					variant="filled"
-					value={idToCall}
-					onChange={(e) => setIdToCall(e.target.value)}
-				/>
-				<div className="call-button">
-					{callAccepted && !callEnded ? (
-						<Button variant="contained" color="secondary" onClick={leaveCall}>
-							End Call
+				<div className="myId">
+					<TextField
+						id="filled-basic"
+						label="Name"
+						variant="filled"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+						style={{ marginBottom: "20px" }}
+					/>
+					<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
+						<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
+							Copy ID
 						</Button>
-					) : (
-						<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
-							<PhoneIcon fontSize="large" />
-						</IconButton>
-					)}
-					{idToCall}
+					</CopyToClipboard>
+
+					<TextField
+						id="filled-basic"
+						label="ID to call"
+						variant="filled"
+						value={idToCall}
+						onChange={(e) => setIdToCall(e.target.value)}
+					/>
+					<div className="call-button">
+						{callAccepted && !callEnded ? (
+							<Button variant="contained" color="secondary" onClick={leaveCall}>
+								End Call
+							</Button>
+						) : (
+							<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
+								<PhoneIcon fontSize="large" />
+							</IconButton>
+						)}
+						{idToCall}
+					</div>
+				</div>
+				<div>
+					{receivingCall && !callAccepted ? (
+						<div className="caller">
+							<h1 >{name} is calling...</h1>
+							<Button variant="contained" color="primary" onClick={answerCall}>
+								Answer
+							</Button>
+						</div>
+					) : null}
 				</div>
 			</div>
 			<div>
-				{receivingCall && !callAccepted ? (
-						<div className="caller">
-						<h1 >{name} is calling...</h1>
-						<Button variant="contained" color="primary" onClick={answerCall}>
-							Answer
-						</Button>
-					</div>
-				) : null}
+				{devices && (
+					<>
+						<label style={fieldStyle}>
+							<span style={labelStyle}>Video device: </span>
+							<select style={selectStyle} disabled={devices.video.length === 0} onChange={handleVideoDeviceChange}>
+								{devices.video.map((device, index) => (
+									<option key={device.deviceId} value={index}>
+										{device.label || device.deviceId || device.groupId}
+									</option>
+								))}
+							</select>
+						</label>
+						<label style={fieldStyle}>
+							<span style={labelStyle}>Audio device: </span>
+							<select style={selectStyle} disabled={devices.audio.length === 0} onChange={handleAudioDeviceChange}>
+								{devices.audio.map((device, index) => (
+									<option key={device.deviceId} value={index}>
+										{device.label || device.deviceId || device.groupId}
+									</option>
+								))}
+							</select>
+						</label>
+					</>
+				)}
 			</div>
-		</div>
 		</>
 	)
 }
