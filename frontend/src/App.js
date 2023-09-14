1fd166c91d43bbead28b00c68fc655ad4f805791
laliux <eduardo@kingtide.com>
2023-09-14 09:27:02 -0600

    Added changes to have user roles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import Button from "@material-ui/core/Button"
 import IconButton from "@material-ui/core/IconButton"
 import TextField from "@material-ui/core/TextField"
-import AssignmentIcon from "@material-ui/icons/Assignment"
 import PhoneIcon from "@material-ui/icons/Phone"
 import React, { useEffect, useRef, useState } from "react"
-import { CopyToClipboard } from "react-copy-to-clipboard"
+import {useLocation} from 'react-router-dom';
 import Peer from "simple-peer"
 import io from "socket.io-client"
 import "./App.css"
@@ -22,7 +21,12 @@ const selectStyle = {
 };
 
 const socket = io.connect('https://socket.bbfour.me')
+//const socket = io.connect('http://localhost:4000');
+
 function App() {
+	const location = useLocation();
+	const role = location.pathname === '/operator' ? 'operator' : 'kiosk';
+
 	const [videoDeviceId, setVideoDeviceId] = React.useState(null);
 	const [audioDeviceId, setAudioDeviceId] = React.useState(null);
 	const [devices, setDevices] = React.useState(null);
@@ -34,7 +38,6 @@ function App() {
 	const [callAccepted, setCallAccepted] = useState(false)
 	const [idToCall, setIdToCall] = useState("")
 	const [callEnded, setCallEnded] = useState(false)
-	const [name, setName] = useState("")
 	const myVideo = useRef()
 	const userVideo = useRef()
 	const connectionRef = useRef()
@@ -63,23 +66,38 @@ function App() {
 	};
 
 	useEffect(() => {
-		const promise = listDevices();  // <--- lists available input audio and input video devices
-		promise
-			.then((devices) => setDevices(devices));
+		console.log("ROLE: ", role);
+
+		listDevices().then((devices) => setDevices(devices));
 
 		navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
 			setStream(stream)
 			myVideo.current.srcObject = stream
+
+			console.log("Setting stream to: ", stream);
 		})
 
 		socket.on("me", (id) => {
+			console.log("ME: ", id);
 			setMe(id)
+
+			if(role === 'operator'){
+				socket.emit("setOperatorId", {
+					operatorId: id,
+				});
+			}
+		})
+
+		socket.on("setOperatorId", (id) => {
+			console.log("The operator Id: ", id);
+			setIdToCall(id);
 		})
 
 		socket.on("callUser", (data) => {
+			console.log("callUser!! ", data);
+			console.log({callAccepted});
 			setReceivingCall(true)
 			setCaller(data.from)
-			setName(data.name)
 			setCallerSignal(data.signal)
 		})
 
@@ -98,9 +116,10 @@ function App() {
 			handleDeviceChange();
 		}
 
-	}, [videoDeviceId, audioDeviceId])
+	}, [videoDeviceId, audioDeviceId, role, receivingCall, callAccepted])
 
 	const callUser = (id) => {
+		setCallEnded(false)
 		const peer = new Peer({
 			initiator: true,
 			trickle: false,
@@ -110,35 +129,50 @@ function App() {
 			socket.emit("callUser", {
 				userToCall: id,
 				signalData: data,
-				from: me,
-				name: name
+				from: me
 			})
 		})
 		peer.on("stream", (stream) => {
-
-			userVideo.current.srcObject = stream
-
+			console.log("callUser() ", {callEnded});
+			console.log("callUser() ", {callAccepted});
+			console.log("callUser() , stream ", {stream});
+			console.log("callUser() userVideo.current ", userVideo.current );
+			if(userVideo && userVideo.current){
+				userVideo.current.srcObject = stream
+			}
 		})
 		socket.on("callAccepted", (signal) => {
 			setCallAccepted(true)
 			peer.signal(signal)
+		})
+		socket.on("callEnded", () => {
+			console.log("CALL Ended ");
+			setCallEnded(true);
 		})
 
 		connectionRef.current = peer
 	}
 
 	const answerCall = () => {
+		setReceivingCall(false)
 		setCallAccepted(true)
+		setCallEnded(false)
 		const peer = new Peer({
 			initiator: false,
 			trickle: false,
 			stream: stream
 		})
+		console.log("answerCall() ", {callAccepted},{callEnded});
+		console.log("answerCall() ", {stream});
 		peer.on("signal", (data) => {
 			socket.emit("answerCall", { signal: data, to: caller })
 		})
 		peer.on("stream", (stream) => {
-			userVideo.current.srcObject = stream
+			console.log("answerCall() on.stream() ", {stream});
+			console.log("answerCall() userVideo.current: ", userVideo.current);
+			if(userVideo && userVideo.current){
+				userVideo.current.srcObject = stream
+			}
 		})
 
 		peer.signal(callerSignal)
@@ -146,8 +180,13 @@ function App() {
 	}
 
 	const leaveCall = () => {
+		console.log("Entering to leaveCall()");
 		setCallEnded(true)
-		connectionRef.current.destroy()
+		setCallAccepted(false)
+		//connectionRef.current.destroy()
+				socket.emit("callEnded", {
+					callerId: caller
+				});
 	}
 
 	const handleVideoDeviceChange = (e) => {
@@ -165,57 +204,52 @@ function App() {
 
 	return (
 		<>
-			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
+			<h1 style={{ textAlign: "center", color: '#fff' }}>FactoryZoom</h1>
 			<div className="container">
 				<div className="video-container">
 					<div className="video">
 						{stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
 					</div>
+					{
+						!callEnded && 
 					<div className="video">
-						{callAccepted && !callEnded ?
-							<video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} /> :
-							null}
+							<video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} /> 
 					</div>
+					}
 				</div>
-				<div className="myId">
-					<TextField
-						id="filled-basic"
-						label="Name"
-						variant="filled"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-						style={{ marginBottom: "20px" }}
-					/>
-					<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-						<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
-							Copy ID
-						</Button>
-					</CopyToClipboard>
-
-					<TextField
-						id="filled-basic"
-						label="ID to call"
-						variant="filled"
-						value={idToCall}
-						onChange={(e) => setIdToCall(e.target.value)}
-					/>
-					<div className="call-button">
-						{callAccepted && !callEnded ? (
-							<Button variant="contained" color="secondary" onClick={leaveCall}>
-								End Call
-							</Button>
-						) : (
-							<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
-								<PhoneIcon fontSize="large" />
-							</IconButton>
-						)}
-						{idToCall}
-					</div>
-				</div>
+				{role === 'kiosk' &&  (
+					<div className="myId">
+						<TextField
+							id="filled-basic"
+							label="ID to call"
+							variant="filled"
+							value={idToCall}
+							onChange={(e) => setIdToCall(e.target.value)}
+						/>
+						<div className="call-button">
+							{ (!callAccepted || callEnded) && (
+								<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
+									<PhoneIcon fontSize="large" />
+								</IconButton>
+							)}
+						</div>
+					</div> 
+				)}
+				{role === 'operator' &&  (
+					<div className="myId">
+						<div className="call-button">
+							{callAccepted && !callEnded && (
+								<Button variant="contained" color="secondary" onClick={leaveCall}>
+									End Call
+								</Button>
+							) }
+						</div>
+					</div> 
+				)}				
 				<div>
-					{receivingCall && !callAccepted ? (
+					{receivingCall ? (
 						<div className="caller">
-							<h1 >{name} is calling...</h1>
+							<h1>Call from Kiosk...</h1>
 							<Button variant="contained" color="primary" onClick={answerCall}>
 								Answer
 							</Button>
--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require("express")
 const http = require("http")
 const app = express()
 const server = http.createServer(app)
+
+let operatorId = null;
+
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		//origin: "http://localhost:3000",
+		origin: "https://chat.bbfour.me",
 		methods: [ "GET", "POST" ]
 	}
 })
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id)
+	console.log("Sending socket id: ", socket.id);
 
 	socket.on("disconnect", () => {
 		socket.broadcast.emit("callEnded")
@@ -23,6 +28,21 @@ io.on("connection", (socket) => {
 	socket.on("answerCall", (data) => {
 		io.to(data.to).emit("callAccepted", data.signal)
 	})
+
+	socket.on("callEnded", (data) => {
+		console.log("callEnded: ", data);
+		io.to(data.callerId).emit("callEnded", data)
+	})
+
+	socket.on("setOperatorId", (data) => {
+		console.log(data);
+		operatorId = data.operatorId;
+		socket.emit("setOperatorId", operatorId);
+	})
+
+	if(operatorId !== null){
+		socket.emit("setOperatorId", operatorId);
+	}
 })
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+server.listen(4000, () => console.log("server is running on port 4000"))
